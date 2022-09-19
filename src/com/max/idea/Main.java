01704ec5ee3626a8770b25674efd6983c395178a
evgeshavinni@gmail.com <113600208+Nauwoo@users.noreply.github.com>
2022-09-19 17:39:53 +0300

    second commit

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -2,6 +2,8 @@ package com.max.idea;
 
 public class Main {
     public static void main(String[] args) {
+        double num1 = 58.1234;
+        byte num2 = 127;
         System.out.println("Hello World!");
     }
 }
