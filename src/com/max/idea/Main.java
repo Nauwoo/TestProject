9ceb4075ebca0b93779fc1227776784f8ae8197d
evgeshavinni@gmail.com <113600208+Nauwoo@users.noreply.github.com>
2022-09-19 18:43:01 +0300

    fourth commit

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -5,10 +5,10 @@ public class Main {
         double num1 = 58.1234;
         byte num2 = 127;
         int[] myArray = {1,3,5,7,9};
-        int numArray = myArray[0];
-        myArray[0]= myArray[4];
-        myArray[4] = numArray;
-        System.out.println("Сумма первого и среднего элементов = " + (myArray[0] +myArray[2]));
+        int numArray = myArray[myArray.length - 1];
+        myArray[4]= myArray[0];
+        myArray[0] = numArray;
+        System.out.println("Сумма первого и среднего элементов = " + (myArray[0] + myArray[myArray.length-3]));
         System.out.println("Hello World!");
     }
 }
