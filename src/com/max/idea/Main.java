3188d22bac48c2f696fadbfb3b8ebf80aeb108fd
evgeshavinni@gmail.com <113600208+Nauwoo@users.noreply.github.com>
2022-09-19 18:32:23 +0300

    third commit

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
         double num1 = 58.1234;
         byte num2 = 127;
+        int[] myArray = {1,3,5,7,9};
+        int numArray = myArray[0];
+        myArray[0]= myArray[4];
+        myArray[4] = numArray;
+        System.out.println("Сумма первого и среднего элементов = " + (myArray[0] +myArray[2]));
         System.out.println("Hello World!");
     }
 }
