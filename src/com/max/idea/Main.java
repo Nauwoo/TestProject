01d9067fe53faf595879cb6312a0135d31ebcc70
evgeshavinni@gmail.com <113600208+Nauwoo@users.noreply.github.com>
2022-09-21 12:52:49 +0300

    sixth commit

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -3,7 +3,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner inDouble = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        double x = inDouble.nextInt();
+        System.out.print("Введите второе число: ");
+        double y = inDouble.nextInt();
+        Scanner inText = new Scanner(System.in);
+        System.out.print("Выберите арифметическую операцию (+,-,/,*): ");
+        String z = inText.nextLine();
+        switch (z) {
+            case "+" : System.out.println("Результат: " + (x + y));
+                break;
+            case "-" : System.out.println("Результат: " + (x - y));
+                break;
+            case "*" : System.out.println("Результат: " + (x * y));
+                break;
+            case "/" : System.out.println("Результат: " + (x / y));
+                break;
+            default :  System.out.println("Некорректная операция");
+        }
+
+
+
+
+
+
+
+
+        /* Scanner in = new Scanner(System.in);
         System.out.print("Введите значение x: ");
         double x = in.nextInt();
         System.out.print("Введите значение y: ");
@@ -17,13 +44,13 @@ public class Main {
         if (b > 3)
             System.out.println("Программа выполнена корректно");
 
-       //double num1 = 58.1234;
-       //byte num2 = 127;
-       //int[] myArray = {1,3,5,7,9};
-       //int numArray = myArray[myArray.length - 1];
-       //myArray[4]= myArray[0];
-       //myArray[0] = numArray;
-       //System.out.println("Сумма первого и среднего элементов = " + (myArray[0] + myArray[myArray.length-3]));
-       //System.out.println("Hello World!");
+       double num1 = 58.1234;
+       byte num2 = 127;
+       int[] myArray = {1,3,5,7,9};
+       int numArray = myArray[myArray.length - 1];
+       myArray[4]= myArray[0];
+       myArray[0] = numArray;
+       System.out.println("Сумма первого и среднего элементов = " + (myArray[0] + myArray[myArray.length-3]));
+       System.out.println("Hello World!"); */
     }
 }
