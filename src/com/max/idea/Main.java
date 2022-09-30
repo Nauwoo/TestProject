7cd1b2b0dd12bc46cd6f4badc334bbedb6980b93
evgeshavinni@gmail.com <113600208+Nauwoo@users.noreply.github.com>
2022-09-30 13:28:32 +0300

    final task 2

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -7,12 +7,38 @@ import java.math.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Укажите курс доллара: ");
-        double rate = in.nextDouble();
-        System.out.print("Введите количество рублей: ");
-        double count = in.nextDouble();
-        String result = String.format("%.2f", count * rate);
-        System.out.print("Итого: " + result);
+        System.out.print("Ввод: ");
+        String text = in.nextLine();
+        char[] test = text.toCharArray();
+        int result = 0;
+        for (int i = 0; i < test.length; i++) {
+            if (Character.isLetter(test[i])) {
+                if (test[1] == '+') {
+                    switch (i) {
+                        case 0:
+                            result += Character.getNumericValue(test[4]) - Character.getNumericValue(test[2]); break;
+                        case 2:
+                            result += Character.getNumericValue(test[4]) - Character.getNumericValue(test[0]); break;
+                        case 4:
+                            result += Character.getNumericValue(test[0]) + Character.getNumericValue(test[2]); break;
+                        default:
+                            System.out.print("Что-то не так"); break;
+                    }
+                } else if (test[1] == '-') {
+                    switch (i) {
+                        case 0:
+                            result += Character.getNumericValue(test[4]) + Character.getNumericValue(test[2]); break;
+                        case 2:
+                            result += Character.getNumericValue(test[0]) - Character.getNumericValue(test[4]); break;
+                        case 4:
+                            result += Character.getNumericValue(test[0]) - Character.getNumericValue(test[2]); break;
+                        default:
+                            System.out.print("Что-то не так"); break;
+                    }
+                }
+            }
+        }
+        System.out.print("Вывод: " + result);
     }
 }
 
