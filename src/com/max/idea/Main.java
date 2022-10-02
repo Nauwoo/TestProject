e8487fb206d44b6011e2b9a4294fc9aee825809b
evgeshavinni@gmail.com <113600208+Nauwoo@users.noreply.github.com>
2022-10-02 12:31:10 +0300

    final task 4

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -6,30 +6,32 @@ import java.math.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Загадка: Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает");
         Scanner in = new Scanner(System.in);
-        System.out.print("Укажите количество строк: ");
-        int count = in.nextInt();
-        String[] stringArray = new String[count];
-        Scanner inLine = new Scanner(System.in);
-        for (int i = 1; i <= count; i++){
-           System.out.print("Введите строку " + i + ": ");
-           stringArray[i-1] = inLine.nextLine();
-        }
-        int max = 0;
-        String temp2 = "";
-        for (int i = 0; i < stringArray.length; i++){
-            int countOfUniqueChars = stringArray[i].length();
-            for (int j = 0; j < stringArray[i].length(); j++){
-                if (j != stringArray[i].indexOf(stringArray[i].charAt(j))){
-                    countOfUniqueChars--;
-                }
+        boolean tip = false;
+        for (int i = 1; i < 4; i++){
+            System.out.print("Попытка " + i + ": ");
+            String answer = in.nextLine();
+            if (answer.toLowerCase().equals("заархивированный вирус")) {
+                System.out.print("Правильно!");
+                break;
             }
-           if (countOfUniqueChars > max) {
-               max = countOfUniqueChars;
-               temp2 = stringArray[i];
-           }
+            else if ((answer.toLowerCase().equals("подсказка")) && (i == 1)) {
+                System.out.println("Это не лук.");
+                tip = true;
+                i--;
+            }
+            else if ((answer.toLowerCase().equals("подсказка")) && (i != 1)) {
+                System.out.println("Подсказка уже недоступна.");
+                i--;
+            }
+            else if ((tip) && (i >= 1)) {
+                System.out.print("Обидно, приходи в другой раз");
+                break;
+            }
+            else if (i == 3)  System.out.print("Обидно, приходи в другой раз");
+            else  System.out.println("Подумай еще!");
         }
-        System.out.print("Ответ: " + temp2);
     }
 }
 
